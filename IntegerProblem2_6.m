491a01ce10edeb876956017944b287fae23bec19
jiangxuzhao <1169872430@qq.com>
2021-12-31 23:12:18 +0800

    linear problem and integer problem

---
+++ IntegerProblem2_6.m
@@ -0,0 +1,10 @@
+clc,clear
+c=load('data2_6.txt');
+prob=optimproblem;
+x=optimvar('x',4,5,'LowerBound',0,'UpperBound',1);
+prob.Objective=sum(sum(c.*x));
+prob.Constraints.con1=sum(x,1)==1;
+prob.Constraints.con2=sum(x,2)<=2;
+[sol,fval]=solve(prob);
+sol.x
+fval
